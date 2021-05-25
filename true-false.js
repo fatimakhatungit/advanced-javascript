bc2ce853d4b6eb4940560b767d4bfac65fb925ca
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-05-25 10:39:08 +0600

    added

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+// '0'
+// let name =NaN 
+// const age = 4;
+// let name = [];
+// let name = false;
+let name = 12;
+// let name = null;
+// console.log(name);
+if(name || name == 0){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
